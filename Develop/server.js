6668305c66de4f646e786e4c3f7954b66036d43b
Volcomix <m.santo13@hotmail.com>
2023-06-04 14:08:46 -0600

    added code to server.js

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const api = require ('./routes/api-routes.js');
+const html = require ('./routes/html-routes.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+//Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+//routes
+app.use(api);
+app.use(html);
+
+app.listen(PORT,()=>
+    console.log(`App listenint at http://localhost:${PORT}`)
+);
+
+
