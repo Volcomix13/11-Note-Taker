e6520f19ef0fb712cc9104ad5f07c32cc56cbc71
Volcomix <m.santo13@hotmail.com>
2023-06-05 20:47:51 -0600

    fixed typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const api = require('./routes/api-routes.js');
 const html = require('./routes/html-routes.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
